1ddb4e4aed547faffb5f992b4aae8e31d6519e03
Paul Todd <pault@fleetway.com>
2017-07-06 14:49:58 +0100

    Capital requirments

--- src/common/calculations/cap-req-calculator.ts
+++ src/common/calculations/cap-req-calculator.ts
@@ -1,6 +1,15 @@
 import * as Gaussian from "gaussian"
+import { Formula } from "./formula"
+import { BetaDist } from "./beta-dist"
 
-export class CapReqCalculator {
+type Reg = {
+   r : {
+      CRR: number,
+      RW: number
+   }
+};
+
+export class CapReqCalculator extends Formula {
    /**
     * 
     * @param pd - probability of default
@@ -8,28 +17,71 @@ export class CapReqCalculator {
     * @param M - 
     * @param s - 
     */
-   static sme(pd: number, lgd: number, M: number, s: number): number {
+
+   static reg = {
+      eu: {
+         CRR: 1.06,
+         RW: 12.5 
+      }
+   };
+
+   static smeBasel(pd: number, lgd: number, M: number, s: number): number {
       let dist = Gaussian(0, 1);
       let R: number = 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.24 * (1 - (1 - Math.exp(-50.0 * pd))) - 0.04 * (1 - (s - 5) / 45);
-      let Ri: number = 1 / (1 - R);
-      let rRi: number = Math.pow(Ri, 0.5);
-      let b: number = Math.pow(0.11852 - 0.05478 * Math.log(pd), 2);
-      return lgd * (dist.cdf(rRi * dist.ppf(pd) + rRi * dist.ppf(0.999)) - pd) * (1 + (M - 2.5) * b) / (1 - 1.5 * b);
+
+      let b: number = Formula.b(pd);
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999)) - pd) * (1 + (M - 2.5) * b) / (1 - 1.5 * b);
    }
 
-   /**
-    * 
-    * @param pd - probability of default
-    * @param lgd - loss given default
-    * @param M - 
-    * @param s - 
-    */
-   static sme2(pd: number, lgd: number, M: number, s: number): number {
+   static smeCRRCapital(pd: number, lgd: number, M: number, s: number, crr: number): number {
+      return CapReqCalculator.smeBasel(pd, lgd, M, s) * crr;
+   }
+
+   static smeRW(pd: number, lgd: number, M: number, s: number, crr: number, rw: number): number {
+      return CapReqCalculator.smeBasel(pd, lgd, M, s) * crr * rw;
+   }
+   
+   static hv_cre(pd: number, lgd: number, M: number, s: number): number {
       let dist = Gaussian(0, 1);
-      let R: number = 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.24 * (1 - (1 - Math.exp(-50.0 * pd))) - 0.04 * (1 - (s - 5) / 45);
-      let Ri: number = 1 / (1 - R);
-      let rRi: number = Math.pow(Ri, 0.5);
-      let b: number = Math.pow(0.11852 - 0.05478 * Math.log(pd), 2);
-      return 100 + (lgd * (dist.cdf(rRi * dist.ppf(pd) + rRi * dist.ppf(0.999)) - pd) * (1 + (M - 2.5) * b) / (1 - 1.5 * b));
+      let R: number = 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.3 * (1 - (1 - Math.exp(-50.0 * pd)));
+      let b: number = Formula.b(pd);
+
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999)) - pd) * (1 + (M - 2.5) * b) / (1 - 1.5 * b);
+   }
+
+   static revolving(pd: number, lgd: number) {
+      let dist = Gaussian(0, 1);
+      let R = 0.4;
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999))) - pd * lgd;
+   }
+
+   static residential(pd: number, lgd: number) {
+      let dist = Gaussian(0, 1);
+      let R = 0.15;
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999))) - pd * lgd;
+   }
+
+   static otherRetail(pd: number, lgd: number) {
+      let dist = Gaussian(0, 1);
+      let R: number = 0.03 * (1 - Math.exp(-35.0 * pd)) / (1 - Math.exp(-35.0)) + 0.16 * (1 - (1 - Math.exp(-35.0 * pd)) / (1 - Math.exp(-35.0)));
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999))) - pd * lgd;
+   }
+
+   static sovPP(pd: number, lgd: number, M: number, pdpp: number): number {
+      let dist = Gaussian(0, 1);
+      let pdmin = Math.min(pd, pdpp);
+      let R: number = 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.24 * (1 - (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)));
+      let b: number = Formula.b(pdmin);
+      return lgd * (dist.cdf(Formula.rR(1, R) * dist.ppf(pd) + Formula.rR(R, R) * dist.ppf(0.999)) - pd) * (1 + (M - 2.5) * b) / (1 - 1.5 * b) + Formula.capitalPP(pdpp);
+   }
+
+   static sov(pd: number, lgd: number) {
+      let dist = Gaussian(0, 1);
+      let R: number = 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.24 * (1 - (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-35.0)));
+      let b: number = Formula.b(pd);
+   }
+
+   static capitalCorrelationSOV(pd: number) {
+      return 0.12 * (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-50.0)) + 0.24 * (1 - (1 - Math.exp(-50.0 * pd)) / (1 - Math.exp(-35.0)));
    }
 }
--- src/common/calculations/cap-req.spec.ts
+++ src/common/calculations/cap-req.spec.ts
@@ -1,12 +1,19 @@
 import { CapReqCalculator } from "../index";
 
 describe('Component: Capital Requirement Calculations', () => {
-   it('should SME calculate SME Cap req', () => {
-      let val = CapReqCalculator.sme(0.03, 0.3, 3, 25);
-      expect(val.toFixed(5)).toBe('0.32059');
+   it('should SME calculate SME Cap Requirements - Basel', () => {
+      let val;
+      val = CapReqCalculator.smeBasel(0.03, 0.45, 3, 5);
+      expect(val.toFixed(5)).toBe('0.08183');
    });
-   it('should SME calculate SME Cap req', () => {
-      let val = CapReqCalculator.sme(0.03, 0.4, 3, 25);
-      expect(val.toFixed(5)).toBe('0.42746');
+   it('should SME calculate SME Cap Requirements - CRR Capital', () => {
+      let val;
+      val = CapReqCalculator.smeCRRCapital(0.03, 0.45, 3, 5, 1.06);
+      expect(val.toFixed(5)).toBe('0.08674');
+   });
+   it('should SME calculate SME Cap Requirements - RW', () => {
+      let val;
+      val = CapReqCalculator.smeRW(0.03, 0.45, 3, 5, 1.06, 12.5);
+      expect(val.toFixed(5)).toBe('1.08422');
    });
 });
