2903d1c4579ca386621fa54baeb17ce3232551fd
Paul Todd <pault@fleetway.com>
2017-07-11 16:44:33 +0100

    better test specs

--- src/common/calculations/supervisory.spec.ts
+++ src/common/calculations/supervisory.spec.ts
@@ -30,4 +30,20 @@ describe('Component: Supervisory Formula', () => {
       console.log(supervisory.log);
       expect(val.toFixed(1)).toBe('12.5');
    });
+
+   // {"formula":"supervisory","K":1.35,"L":3.55,"detach":0.75,"N":150,"LGD":0.45}
+   it('should calculate: 100% capital i.e 12.5 RW', () => {
+      let supervisory = new Supervisory();
+      let K: number = 0.12;
+      let L: number = 0.03;
+      let detach: number = 0.75;
+      let N: number = 150;
+      let LGD: number = 0.45;
+      let tau = 1000;
+      let omega = 20;
+      let val;
+      val = supervisory.calc(K, L, detach, N, LGD, tau, omega);
+      console.log(supervisory.log);
+      expect(val.toFixed(4)).toBe('1.7683');
+   });
 });
